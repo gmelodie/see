a669824b4d1e70956039eecd458d9be742bb26e5
Gabriel Cruz <gabe@gmelodie.com>
2025-10-28 15:44:33 -0300

    add skeleton of script

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,31 @@
+use anyhow::Result;
+use crossterm::terminal;
+use image::ImageReader;
+use std::env;
+use std::path::Path;
+
+struct AsciiImg {
+    rows: usize,
+    cols: usize,
+    image: Vec<char>,
+}
+
+fn see(file: &Path, term_rows: usize, term_cols: usize) -> Result<AsciiImg> {
+    let img = ImageReader::open("myimage.png")?
+        .with_guessed_format()?
+        .decode()?;
+    // open file
+}
+
 fn main() {
-    println!("Hello, world!");
+    let (trows, tcols) = terminal::size().unwrap();
+    // read file paths
+    for (i, arg) in env::args().enumerate() {
+        // ignore progname
+        if i == 0 {
+            continue;
+        }
+        println!("{arg}");
+        see(Path::new(&arg), trows.into(), tcols.into());
+    }
 }
