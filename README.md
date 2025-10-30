105eb75aaad1e69568de5be42921d3479e5594c8
Gabriel Cruz <gabe@gmelodie.com>
2025-10-30 20:26:15 -0300

    fix rest of code

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # see
 Convert images to ascii on your terminal
+
+```bash
+cargo run -- test.png
+```
--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,59 @@
 use anyhow::Result;
 use crossterm::terminal;
-use image::ImageReader;
-use std::env;
-use std::path::Path;
+use image::{imageops::FilterType, DynamicImage, ImageReader};
+use std::{env, fmt, path::Path, process::exit};
+
+const ASCII_GRADIENT: &[u8] = b"@%#*+=-:. "; // darkest → lightest
 
 struct AsciiImg {
-    rows: usize,
-    cols: usize,
+    width: u32,
     image: Vec<char>,
 }
 
-fn see(file: &Path, term_rows: usize, term_cols: usize) -> Result<AsciiImg> {
-    let img = ImageReader::open("myimage.png")?
-        .with_guessed_format()?
-        .decode()?;
-    // open file
+impl fmt::Display for AsciiImg {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        for (i, p) in self.image.iter().enumerate() {
+            if i > 0 && i % self.width as usize == 0 {
+                write!(f, "\n")?;
+            }
+            write!(f, "{p}")?;
+        }
+        Ok(())
+    }
 }
 
-fn main() {
-    let (trows, tcols) = terminal::size().unwrap();
+impl From<DynamicImage> for AsciiImg {
+    fn from(img: DynamicImage) -> Self {
+        AsciiImg {
+            width: img.width(),
+            image: img.as_bytes().iter().map(|b| pixel_to_ascii(*b)).collect(),
+        }
+    }
+}
+
+fn pixel_to_ascii(byte: u8) -> char {
+    let index = (byte as f32 / 255.0 * (ASCII_GRADIENT.len() - 1) as f32) as usize;
+    ASCII_GRADIENT[index] as char
+}
+
+fn see(file: &Path, term_rows: u32, term_cols: u32) -> Result<()> {
+    let dyn_img = ImageReader::open(file)?
+        .with_guessed_format()?
+        .decode()?
+        .resize(term_cols, term_rows, FilterType::Gaussian)
+        .grayscale();
+    let img = AsciiImg::from(dyn_img);
+    println!("{img}");
+    // TODO: print the image
+    Ok(())
+}
+
+fn main() -> Result<()> {
+    if env::args().len() != 2 {
+        println!("usage: cargo run -- PATH");
+        exit(1);
+    }
+    let (tcols, trows) = terminal::size().unwrap();
     // read file paths
     for (i, arg) in env::args().enumerate() {
         // ignore progname
@@ -26,6 +61,7 @@ fn main() {
             continue;
         }
         println!("{arg}");
-        see(Path::new(&arg), trows.into(), tcols.into());
+        see(Path::new(&arg), trows.into(), tcols.into())?;
     }
+    Ok(())
 }
